c2960401dbb839b542de0e5a851083c14e1bc83c
Vazelek <valentinherve60@gmail.com>
2024-05-24 15:02:10 +0200

    Add login page tests

--- tests/WindowGenerationTests.cpp
+++ tests/WindowGenerationTests.cpp
@@ -1,6 +1,9 @@
 #include "WindowGenerationTests.h"
 
 void WindowGenerationTests::initTestCase() {
+    stacked_widgets = nullptr;
+    login = nullptr;
+
     mainWindow = new MainWindow();
     mainWindow->show();
 }
@@ -11,9 +14,55 @@ void WindowGenerationTests::test_WindowWellGenerated()
     QCOMPARE(mainWindow->windowTitle(), QString("Joffrey's Journey"));
 }
 
-void WindowGenerationTests::test_ShieldSuppressArrow()
-{
-    //QTest::mouseClick
+void WindowGenerationTests::test_WindowCreatedOnLoginPage() {
+    stacked_widgets = mainWindow->getStackedWidget()->currentWidget();
+    QCOMPARE(stacked_widgets->metaObject()->className(), QString::fromStdString("Login"));
+}
+
+void WindowGenerationTests::test_UsernameInputField() {
+    if (stacked_widgets->metaObject()->className() != QString::fromStdString("Login")) {
+        QSKIP("Previous failed test is required");
+        return;
+    }
+
+    login = (Login*) stacked_widgets;
+    QLineEdit* input = login->getInput();
+
+    QVERIFY(input != nullptr);
+
+    if (input != nullptr) {
+        QTest::keyClicks(input, "TestUsername");
+        QCOMPARE(input->text(), QString::fromStdString("TestUsername"));
+    }
+}
+
+void WindowGenerationTests::test_NewConnectionMessageOnConnectButtonClick() {
+    if (login == nullptr) {
+        QSKIP("Previous failed test is required");
+        return;
+    }
+
+    QTest::mouseClick(login->getLoginButton(), Qt::LeftButton);
+    QLabel* infos = login->getInfosLabel();
+
+    QVERIFY(infos != nullptr);
+
+    if (infos != nullptr) {
+        QCOMPARE(
+            infos->text(),
+            QString::fromStdString("You are about to log in as \"TestUsername\"\nYour scores and records will be saved under this nickname")
+        );
+    }
+}
+
+void WindowGenerationTests::test_WindowSwitchToMenuOnNewConnectionButtonClicked() {
+    if (login == nullptr) {
+        QSKIP("Previous failed test is required");
+        return;
+    }
+
+    QTest::mouseClick(login->getNewConnectionButton(), Qt::LeftButton);
+    QCOMPARE(mainWindow->getStackedWidget()->currentWidget()->metaObject()->className(), QString::fromStdString("Menu"));
 }
 
 void WindowGenerationTests::cleanupTestCase() {
--- tests/WindowGenerationTests.h
+++ tests/WindowGenerationTests.h
@@ -4,6 +4,7 @@
 #include <QtTest/QtTest>
 #include <QObject>
 #include "MainWindow.h"
+#include "Login.h"
 
 class WindowGenerationTests : public QObject
 {
@@ -11,12 +12,23 @@ class WindowGenerationTests : public QObject
 
 private:
     MainWindow* mainWindow;
+    QWidget* stacked_widgets;
+    Login* login;
 
 private slots:
+    // Init
     void initTestCase();
     void test_WindowWellGenerated();
-    void test_ShieldSuppressArrow();
 
+    // Login page
+    void test_WindowCreatedOnLoginPage();
+    void test_UsernameInputField();
+    void test_NewConnectionMessageOnConnectButtonClick();
+
+    // Menu page
+    void test_WindowSwitchToMenuOnNewConnectionButtonClicked();
+
+    // Cleanup
     void cleanupTestCase();
 };
 
