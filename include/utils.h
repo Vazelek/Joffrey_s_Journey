d68527647fd54bcc3ddb14d19874822007662a39
Vazelek <valentinherve60@gmail.com>
2024-06-30 18:58:03 +0200

    Implement getIceSpeed and isEntityFarFromPlayer functions

--- include/utils.h
+++ include/utils.h
@@ -1,3 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <tuple>
+#include <QVector>
+#include <iostream>
+#include <cmath>
+
 bool collidesWithPlatform(
     int platform_x, 
     int platform_y, 
@@ -11,3 +19,20 @@ bool collidesWithPlatform(
     int entity_h_speed
 );
 
+std::tuple<float, float> getIceSpeed(float hori_speed, float ice_speed, bool is_on_ice);
+
+bool isEntityFarFromPlayer(
+    int player_x, 
+    int player_y, 
+    int entity_x, 
+    int entity_y, 
+    int player_width, 
+    int player_height,
+    int entity_width, 
+    int entity_height,
+    int minimum_distance
+);
+
+double distance(int x1, int y1, int x2, int y2);
+
+#endif
